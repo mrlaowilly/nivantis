7953204a47c7d84b9d8a43f77f2bf7d8ae31688c
mrlaowilly <willy.meksavanh@hotmail.fr>
2020-06-05 10:53:38 +0200

    fonctionnalité version1

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import logo from './assets/logo.jpg';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import DetailsScree from "./component/DetailsScreen"
 
 function HomeScreen({ navigation }) {
     return (
@@ -26,15 +27,6 @@ Version 2: Détails officines et formulaires
     );
 }
 
-function DetailsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Ecran de la version</Text>
-            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-        </View>
-    );
-}
-
 function Version2Screen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -48,66 +40,66 @@ const Stack = createStackNavigator();
 
 export default function App() {
 
-  return (
-      <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-              <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Acceuil Nivantis DMO' }} />
-              <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Version 1' }} />
-              <Stack.Screen name="Version2" component={Version2Screen} options={{ title: 'Version 2' }} />
-          </Stack.Navigator>
-          <Image source={logo} style={styles.logo} />
-          <Text style={styles.instructions}>
-{`
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Acceuil Nivantis DMO' }} />
+                <Stack.Screen name="Details" component={DetailsScree} options={{ title: 'Version 1' }} />
+                <Stack.Screen name="Version2" component={Version2Screen} options={{ title: 'Version 2' }} />
+            </Stack.Navigator>
+            <Image source={logo} style={styles.logo} />
+            <Text style={styles.instructions}>
+                {`
 Version 1: Détails médicaments
 Version 2: Détails officines et formulaires
 `}
-          </Text>
-          <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
-              <Text style={styles.buttonText}>Accès version 1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
-              <Text style={styles.buttonText}>Accès version 2</Text>
-          </TouchableOpacity>
-          <Text style={styles.instructions}>
-              A propos / Notice / Infos légales
-          </Text>
-      </NavigationContainer>
-  );
+            </Text>
+            <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+                <Text style={styles.buttonText}>Accès version 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+                <Text style={styles.buttonText}>Accès version 2</Text>
+            </TouchableOpacity>
+            <Text style={styles.instructions}>
+                A propos / Notice / Infos légales
+            </Text>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-/*  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-*/
-  logo: {
-    width: 305,
-    height: 55,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#888',
-    fontSize: 18,
-    marginHorizontal: 15,
-    marginBottom: 25,
-    marginTop: 25,
-  },
-  button: {
-    backgroundColor: "steelblue",
-    padding: 20,
-    borderRadius: 5,
-    marginBottom: 25,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: "cover"
-  },
+    /*  container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    */
+    logo: {
+        width: 305,
+        height: 55,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#888',
+        fontSize: 18,
+        marginHorizontal: 15,
+        marginBottom: 25,
+        marginTop: 25,
+    },
+    button: {
+        backgroundColor: "steelblue",
+        padding: 20,
+        borderRadius: 5,
+        marginBottom: 25,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
+    thumbnail: {
+        width: 300,
+        height: 300,
+        resizeMode: "cover"
+    },
 });
---
+++ component/DetailsScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, Text, View, ListView, Button,SafeAreaView, ScrollView  } from 'react-native';
+import Medic from "../assets/data/medicaments.json"
+import {Link} from "@react-navigation/native";
+
+export default class DetailsScreen extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: Medic,
+        }
+    }
+
+    renderList() {
+        return this.state.data.map(i => {
+            var bru = i.price-(i.price * 23)/100
+            var brut = bru.toFixed(2)
+            var remis = (1- i.price/ brut) * 100
+            var remise = remis.toFixed(2)
+            var coef = i.price / 0.2;
+            var coeff = coef.toFixed(2)
+            var vent = i.price * 0.2
+            var vente = vent.toFixed(2)
+            return (
+                <Link key={i.id} style={styles.welcome}>
+                    {i.title}
+                </Link>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <ScrollView style={styles.scrollView}>
+                {this.renderList()}
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#794BFF",
+    },
+    welcome: {
+        fontSize: 15,
+        textAlign: "center",
+        margin: 120,
+    },
+});
