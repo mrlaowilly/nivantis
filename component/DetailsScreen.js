d2943aed19cc6182f52a0163dde35218ba6c62fd
VanPen <van.pen@epitech.eu>
2020-06-05 17:33:14 +0200

    Maj v1

--- component/DetailsScreen.js
+++ component/DetailsScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, ListView, Button,SafeAreaView, ScrollView  } from 'react-native';
+import { StyleSheet, Text, View, ListView, Button,TouchableOpacity, ScrollView, Alert  } from 'react-native';
 import Medic from "../assets/data/medicaments.json"
 import {Link} from "@react-navigation/native";
+
+
 
 export default class DetailsScreen extends React.Component{
     //props pour recuprer les variables
@@ -12,10 +14,31 @@ export default class DetailsScreen extends React.Component{
         }
     }
 
+    renderById(id){
+        return this.state.data.map(i => {
+            var bru = i.price-(i.price * 22)/100
+            var brut = bru.toFixed(2)
+            var remis = (1- i.price/ brut) * 100
+            var remise = remis.toFixed(2)
+            var coef = i.price / 0.2;
+            var coeff = coef.toFixed(2)
+            var vent = i.price * 0.2
+            var vente = vent.toFixed(2)
+            if (id === i.id){
+                Alert.alert(
+                    'Information complementaire: ',
+                    'Coeff = '+ coeff +'€, Remise = ' +remise+ 'Vente Net ='+vente+'€',
+
+                )
+            }
+
+        })
+    }
+
     renderList() {
         //map pour parcourir un objet i 
         return this.state.data.map(i => {
-            var bru = i.price-(i.price * 23)/100
+            var bru = i.price-(i.price * 22)/100
             var brut = bru.toFixed(2)
             var remis = (1- i.price/ brut) * 100
             var remise = remis.toFixed(2)
@@ -24,12 +47,11 @@ export default class DetailsScreen extends React.Component{
             var vent = i.price * 0.2
             var vente = vent.toFixed(2)
             return (
-                <Link key={i.id} style={styles.welcome}>
-                    {i.title} prix = {i.price} €
-                    brut = {brut} remise = {remise}  €
-                    coef = {coeff} €
-                    Vente net = {vente} €
-                </Link>
+
+                <TouchableOpacity key={i} style={styles.welcome} onPress={() => this.renderById(i.id)} >
+                    <Text>{i.title}</Text>
+                </TouchableOpacity>
+
             )
         })
     }
