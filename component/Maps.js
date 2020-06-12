9d86df3cc1c735c8a9847643f2bc4c5aebe722c7
VanPen <van.pen@epitech.eu>
2020-06-12 10:51:25 +0200

    Firebase

--- component/Maps.js
+++ component/Maps.js
@@ -1,15 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Platform, Text, View, StyleSheet,TouchableOpacity, Button,Alert } from 'react-native';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import MapView, {Callout, Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import Pharmacie from '../assets/data/carte-des-pharmacies-de-paris'
+import Form from "./Form.js";
+
+import * as  firebase from 'firebase';
+import 'firebase/firestore';
 
 export default function Maps() {
+
+    const FirebaseConfig = {
+        apiKey: "AIzaSyDZCGCUnST8S9PI1HIonxY6wWCJ0MacASU",
+        authDomain: "vivantis-ea16a.firebaseapp.com",
+        databaseURL: "https://vivantis-ea16a.firebaseio.com",
+        projectId: "vivantis-ea16a",
+        storageBucket: "vivantis-ea16a.appspot.com",
+        messagingSenderId: "410708130758",
+        appId: "1:410708130758:web:ec7cbcbf245dd5055150c7",
+        measurementId: "G-WS916B3L2V"
+    };
 
     let data = Pharmacie;
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
+    let db;
+    setTimeout(() => {
+        if (!firebase.apps.length) {
+            try {
+                firebase.initializeApp(FirebaseConfig)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        db = firebase.firestore();
+    }, 1000)
+
+
 
     useEffect(() => {
         if (Platform.OS === 'android' && !Constants.isDevice) {
@@ -34,7 +66,9 @@ export default function Maps() {
         }
     });
 
-
+    function form(name){
+        console.log(firebase.database().ref('/test/').push('kilianaizen'))
+    }
 
     let text = 'Waiting..';
     let mylat = '';
@@ -51,12 +85,13 @@ export default function Maps() {
     function pharma() {
         let latt = null;
         let long = null;
+        let map= null;
         return data.map( i => {
-            latt = i.geometry.coordinates[1]
-            long = i.geometry.coordinates[0]
+            latt = i.geometry.coordinates[1];
+            long = i.geometry.coordinates[0];
 
-            let map;
             if(i.fields.complrs === undefined) {
+
                 return (
                     map = <Marker coordinate={{latitude: 0, longitude: 0}}>
                         <Callout>
@@ -68,11 +103,12 @@ export default function Maps() {
             else{
                 return (
                     map = <Marker coordinate={{latitude: latt, longitude: long}}>
-                        <Callout>
-                            <Text style={styles.paragraph}>{i.fields.complrs}</Text>
-                            <Text style={styles.paragraph}>telephone:0{i.fields.telephone}</Text>
-                            <Text style={styles.paragraph}>Adresse : {i.fields.numvoie} {i.fields.typvoie} {i.fields.voie} {i.fields.cp}</Text>
-                            <Text>Plus d'information</Text>
+                        <Callout onPress={() => form(i.fields.complrs)}>
+                                <Text style={styles.paragraph}>{i.fields.complrs}</Text>
+                                <Text style={styles.paragraph}>telephone : +331{i.fields.telephone}</Text>
+                                <Text style={styles.paragraph}>Adresse : {i.fields.numvoie} {i.fields.typvoie} {i.fields.voie} {i.fields.cp}</Text>
+                                <Button variant="primary" title={i.fields.complrs} onPress={() => form(i.fields.complrs)}>
+                                </Button>
                         </Callout>
                     </Marker>
                 )
