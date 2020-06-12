dce1057bc14f0fa7a93dc389f0205ccdf7c0614b
VanPen <van.pen@epitech.eu>
2020-06-12 10:53:22 +0200

    test

--- component/DetailsScreen.js
+++ component/DetailsScreen.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, ListView, Button,TouchableOpacity, ScrollView, Alert  } from 'react-native';
+import { StyleSheet, Text,TouchableOpacity, ScrollView, Alert  } from 'react-native';
 import Medic from "../assets/data/medicaments.json"
-import {Link} from "@react-navigation/native";
-
-
 
 export default class DetailsScreen extends React.Component{
     //props pour recuprer les variables
@@ -28,24 +25,14 @@ export default class DetailsScreen extends React.Component{
                 Alert.alert(
                     'Information complementaire: ',
                     'Coeff = '+ coeff +'€, Remise = ' +remise+ 'Vente Net ='+vente+'€',
-
                 )
             }
-
         })
     }
 
     renderList() {
         //map pour parcourir un objet i 
         return this.state.data.map(i => {
-            var bru = i.price-(i.price * 22)/100
-            var brut = bru.toFixed(2)
-            var remis = (1- i.price/ brut) * 100
-            var remise = remis.toFixed(2)
-            var coef = i.price / 0.2;
-            var coeff = coef.toFixed(2)
-            var vent = i.price * 0.2
-            var vente = vent.toFixed(2)
             return (
 
                 <TouchableOpacity key={i} style={styles.welcome} onPress={() => this.renderById(i.id)} >
