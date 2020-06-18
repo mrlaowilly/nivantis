a56f7a33c170f292bec27720f27957aeb9e7809c
mrlaowilly <willy.meksavanh@hotmail.fr>
2020-06-18 21:33:53 +0200

    ajout banniere cookie

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Image, StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, View, TouchableOpacity, Button, AppRegistry } from 'react-native';
 import logo from './assets/logo.jpg';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Banner } from 'react-native-paper';
 
 import DetailsScree from "./component/DetailsScreen"
 import Maps from "./component/Maps";
 import Form from "./component/Form";
+import Medic from "./assets/data/medicaments";
 
 
 function HomeScreen({ navigation }) {
@@ -54,18 +56,46 @@ function Version2Screen({ navigation }) {
 
 const Stack = createStackNavigator();
 
-export default function App() {
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Acceuil Nivantis DMO' }} />
-                <Stack.Screen name="Details" component={DetailsScree} options={{ title: 'Version 1' }} />
-                <Stack.Screen name="Version2" component={Maps} options={{ title: 'Version 2' }} />
-                <Stack.Screen name="Version3" component={Form} options={{ title: 'Version 3' }} />
-            </Stack.Navigator>
-            <TouchableOpacity
-                onPress={() => alert(`
+export default class MyComponent extends React.Component {
+    state = {
+        visible: true,
+    };
+    render() {
+        return (
+            <NavigationContainer>
+                <Banner
+                    style={{ backgroundColor: 'wheat', marginTop: 30 }}
+                    visible={this.state.visible}
+                    actions={[
+                        {
+                            label: 'Accepter les cookies',
+                            onPress: () => this.setState({ visible: false }),
+                        },
+                        {
+                            label: 'Refuser',
+                            onPress: () => this.setState({ visible: false }),
+                        },
+                    ]}
+                    icon={({ size }) =>
+                        <Image
+                            source={{ uri: 'https://www.latableadessert.fr/sites/default/files/ct/corner_page_recipes_inspiration/image/ltad-picto-cookie.png' }}
+                            style={{
+                                width: size,
+                                height: size,
+                            }}
+                        />
+                    }
+                >
+                    En acceptant, vous déclarer etre consentant pour la collecte de données, blablabla, si vous refusé vous irez en enfer.
+                </Banner>
+                <Stack.Navigator initialRouteName="Home">
+                    <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Acceuil Nivantis DMO' }} />
+                    <Stack.Screen name="Details" component={DetailsScree} options={{ title: 'Version 1' }} />
+                    <Stack.Screen name="Version2" component={Maps} options={{ title: 'Version 2' }} />
+                    <Stack.Screen name="Version3" component={Form} options={{ title: 'Version 3' }} />
+                </Stack.Navigator>
+                <TouchableOpacity
+                    onPress={() => alert(`
 A propos
 Texte a propos de Nivantis
 Notice
@@ -73,12 +103,12 @@ Texte a propos de la notice
 Infos légales
 Texte a propos de l'information légales
 `)}
-                style={{ backgroundColor: 'lightgrey' }}>
-                <Text style={{ fontSize: 20, color: '#fff' }}>A propos / Notice / Infos légales</Text>
-            </TouchableOpacity>
-            <Image source={logo} style={styles.logo} />
-        </NavigationContainer>
-    );
+                    style={{ backgroundColor: 'lightgrey' }}>
+                    <Text style={{ fontSize: 20, color: '#fff' }}>A propos / Notice / Infos légales</Text>
+                </TouchableOpacity>
+                <Image source={logo} style={styles.logo} />
+            </NavigationContainer>
+        );    }
 }
 
 const styles = StyleSheet.create({
